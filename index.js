29c8f4b068f0bb8849f3285a0793bd6be6ac696b
kittimaratana <kittima.ratana@gmail.com>
2024-07-10 16:46:47 -0400

    Add image multer to server

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 5001;
-const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
 const articlesRoutes = require('./routes/articles-routes');
 const projectsRoutes = require('./routes/projects-routes');
+
+//allows request to get static assets in /public folder
+app.use(express.static("public"));
+
+//enable PORT AND cors to define server and client environment variables
+const PORT = process.env.PORT || 5001;
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3000";
 
 // Express middleware
 app.use(express.json());
 app.use(cors({ origin: CORS_ORIGIN }));
 app.use((req, _res, next) => {
     next();
-
 });
 
 // // Configuring blog and konnect endpoints
--- seeds/02_projects.js
+++ seeds/02_projects.js
@@ -12,63 +12,63 @@ exports.seed = async function (knex) {
       project_title: 'Video Player',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "React, JavaScript, HTML, SCSS, REST, Node, Express, Knex, SQL, JSON, Multer, Git",
-      image: '../public/images/brainflix.jpg'
+      image: '/images/brainflix.jpg'
     },
     {
       id: 2,
       project_title: 'Drivers License',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "Python, CNN, Feature Extraction, Neural Networks",
-      image: '../public/images/license.jpg'
+      image: '/images/license.jpg'
     },
     {
       id: 3,
       project_title: 'Travel Site',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "HTML, JavaScript, CSS",
-      image: '../public/images/travel.jpg'
+      image: '/images/travel.jpg'
     },
     {
       id: 4,
       project_title: 'Clustering Analysis',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "Python, Docker, GCP, SQL, Git",
-      image: '../public/images/clusters.jpg'
+      image: '/images/clusters.jpg'
     },
     {
       id: 5,
       project_title: 'Inventory Management System',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "React, JavaScript, HTML, SCSS, REST, Node, Express, Knex, SQL, JSON, Multer, Git",
-      image: '../public/images/inventory.jpg'
+      image: '/images/inventory.jpg'
     },
     {
       id: 6,
       project_title: 'Text Generator',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "Python, RNN, Neural Networks, GenAI, GCP, SQL",
-      image: '../public/images/generator.jpg'
+      image: '/images/generator.jpg'
     },
     {
       id: 7,
       project_title: 'Activites Website',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "JavaScript, HTML, CSS, Web APIs, JSON, Git",
-      image: '../public/images/activities.jpg'
+      image: '/images/activities.jpg'
     },
     {
       id: 8,
       project_title: 'Cost Predictions',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "Python, Regression, GCP, SQL",
-      image: '../public/images/prediction.jpg'
+      image: '/images/prediction.jpg'
     },
     {
       id: 9,
       project_title: 'Flyers Extraction',
       description: "Create video player website to upload and watch new recommended videos",
       tech_stack: "Python, Tesseract OCR, Feature Detection, Sentiment Analysis",
-      image: '../public/images/flyers.jpg'
+      image: '/images/flyers.jpg'
     }
   ]);
 
